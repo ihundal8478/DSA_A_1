0f112e97c6ac86be758130edd4406960cfaab4bb
ihundal <ihundal8478@conestogac.on.ca>
2026-06-20 21:23:41 -0400

    Implemented enqueue function

--- D_S_A/Queue.c
+++ D_S_A/Queue.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "queue.h"
+#include "Queue.h"
 
 void initQueue(Queue* q)
 {
@@ -12,4 +12,21 @@ void initQueue(Queue* q)
 int isEmpty(Queue* q)
 {
     return q->head == NULL;
+}
+void enqueue(Queue* q, User user) {
+    QueueNode* newnode = (QueueNode*)malloc(sizeof(QueueNode));
+    if (newnode == NULL) {
+        return 1;
+    }
+    newnode->data = user;
+    newnode->next = NULL;
+    if (isEmpty(q)) {
+        q->head = newnode;
+        q->tail = newnode;
+    }
+    else {
+        q->tail->next = newnode;
+        q->tail = newnode;
+    }
+
 }
