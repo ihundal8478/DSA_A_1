a5b52c3547b7df2053760efcc771101a376680c8
ihundal <ihundal8478@conestogac.on.ca>
2026-06-20 20:59:24 -0400

    created the structures and added function prototypes

--- D_S_A/Queue.h
+++ D_S_A/Queue.h
@@ -1,1 +1,29 @@
 #pragma once
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef struct
+{
+    char username[11];
+    int level;
+    char faction[10];
+} User;
+
+typedef struct QueueNode
+{
+    User data;
+    struct QueueNode* next;
+} QueueNode;
+
+typedef struct
+{
+    QueueNode* head;
+    QueueNode* tail;
+} Queue;
+
+void initQueue(Queue* q);
+int isEmpty(Queue* q);
+void enqueue(Queue* q, User user);
+User dequeue(Queue* q);
+int addRandomUsers(Queue* q, int count);
+#endif
